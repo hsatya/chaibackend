8512e63fdb1b370f0383c19d014f82b4de970a78
Harshit Satya <harshitsatya5@gmail.com>
2023-12-21 12:05:30 +0530

    added json code

--- index.js
+++ index.js
@@ -3,6 +3,41 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+const githubData = {
+  login: "hsatya",
+  id: 26687659,
+  node_id: "MDQ6VXNlcjI2Njg3NjU5",
+  avatar_url: "https://avatars.githubusercontent.com/u/26687659?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/hsatya",
+  html_url: "https://github.com/hsatya",
+  followers_url: "https://api.github.com/users/hsatya/followers",
+  following_url: "https://api.github.com/users/hsatya/following{/other_user}",
+  gists_url: "https://api.github.com/users/hsatya/gists{/gist_id}",
+  starred_url: "https://api.github.com/users/hsatya/starred{/owner}{/repo}",
+  subscriptions_url: "https://api.github.com/users/hsatya/subscriptions",
+  organizations_url: "https://api.github.com/users/hsatya/orgs",
+  repos_url: "https://api.github.com/users/hsatya/repos",
+  events_url: "https://api.github.com/users/hsatya/events{/privacy}",
+  received_events_url: "https://api.github.com/users/hsatya/received_events",
+  type: "User",
+  site_admin: false,
+  name: "Harshit Satya",
+  company: "Systango",
+  blog: "",
+  location: "Indore, Madhya Pradesh, India",
+  email: null,
+  hireable: null,
+  bio: "I am a Full Stack developer and a teacher.",
+  twitter_username: "HarshitSatya",
+  public_repos: 50,
+  public_gists: 0,
+  followers: 0,
+  following: 1,
+  created_at: "2017-03-26T10:49:03Z",
+  updated_at: "2023-12-21T05:54:33Z",
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -17,6 +52,10 @@ app.get("/login", (req, res) => {
 
 app.get("/youtube", (req, res) => {
   res.send("<h1>Chai aur Code</h1>");
+});
+
+app.get("/github", (req, res) => {
+  res.json(githubData);
 });
 
 app.listen(process.env.PORT, () => {
